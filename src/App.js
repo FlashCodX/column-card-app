675a7f6e94b47e409ca112d6963efe487b9ba95c
FlashCodX <luis.pinto.trading@gmail.com>
2022-04-12 00:09:07 +0100

    Initital Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styles from "./styles/App.module.css";
+const sedans = require("./assets/icon-sedans.svg").default;
+const suvs = require("./assets/icon-suvs.svg").default;
+const luxury = require("./assets/icon-luxury.svg").default;
+const { app, card, contentContainer, learnMoreBtn, attribution } = styles;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className={app}>
+      <main className={card}>
+        <div className={contentContainer}>
+          <img src={sedans} alt="carimage" />
+          <h1>Sedans</h1>
+          <p>
+            Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip.
+          </p>
+          <button className={learnMoreBtn}>Learn More</button>
+        </div>
+        <div className={contentContainer}>
+          <img src={suvs} alt="carimage" />
+          <h1>Suvs</h1>
+          <p>
+            Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacation and off-road adventures.
+          </p>
+          <button className={learnMoreBtn}>Learn More</button>
+        </div>
+        <div className={contentContainer}>
+          <img src={luxury} alt="carimage" />
+          <h1>Luxury</h1>
+          <p>
+            Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style.
+          </p>
+          <button className={learnMoreBtn}>Learn More</button>
+        </div>
+      </main>
+
+      <div className={attribution}>
+        Challenge by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/luispintodev/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Luís Pinto
+        </a>
+        .
+      </div>
     </div>
   );
 }
